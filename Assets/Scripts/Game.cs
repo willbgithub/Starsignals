7e86bcf3f2f915afadeecd322c7a99076dea2d15
Will Jenkins <wiljenki@gmail.com>
2026-04-06 14:55:44 -0400

    renamed main scene to game

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Game : MonoBehaviour
@@ -6,6 +7,6 @@ public class Game : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        cell.SetCell(58);
+        cell.SetCell(0);
     }
 }
